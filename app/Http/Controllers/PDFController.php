db2f00db91258ecc177506b371dbb8ee24ee92b7
hung <hungdangcap99@gmail.com>
2025-04-02 13:42:29 +0700

    update test pdf to image

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\PdfToText\Pdf;
+#use Spatie\PdfToText\Pdf;
+use Spatie\PdfToImage\Pdf;
+
 class PDFController extends Controller
 {
     public function index()
@@ -13,10 +15,29 @@ class PDFController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'file'=>'required|mimes:pdf|max:2048'
+            'file'=>'required|mimes:pdf|max:10240'
         ]);
         $file=$request->file('file');
-        $text= "test";
-        return back()->with(['text'=>$text]);
+        // $text= (new Pdf())
+        // ->setPdf($file)
+        // ->text();
+        $pdfUrl = "https://arxiv.org/pdf/2408.09869"; // URL của file PDF
+    $pdfPath = storage_path('app/public/input.pdf'); // Lưu vào storage
+
+    // Tải file PDF từ URL
+    $response = Http::get($pdfUrl);
+    file_put_contents($pdfPath, $response->body());
+
+    // Chuyển PDF thành ảnh
+    $imagePath = storage_path('app/public/output.jpg');
+    $pdf = new Pdf($pdfPath);
+    $pdf->saveImage($imagePath);
+    return response()->json([
+        'message' => 'PDF converted to image successfully!',
+        'image_url' => asset('storage/output.jpg')
+    ]);
+
+        // dd($text);
+        // return back()->with(['text'=>$text]);
     }
 }
